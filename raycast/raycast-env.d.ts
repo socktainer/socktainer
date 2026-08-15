18377995ae42690b4e03144c8ae22925b4c06068
naaiyy <naaiyyyy@gmail.com>
2026-08-15 20:35:29 +0200

    refactor(raycast): use Glass Dock command slug
    Signed-off-by: naaiyy <naaiyyyy@gmail.com>

--- raycast/raycast-env.d.ts
+++ raycast/raycast-env.d.ts
@@ -11,15 +11,16 @@ type ExtensionPreferences = {
   /** glassdockctl Executable - Optional absolute path to glassdockctl. Standard install locations are detected automatically. */
   "controlExecutable"?: string
 }
+
 /** Preferences accessible in all the extension's commands */
 declare type Preferences = ExtensionPreferences
 
 declare namespace Preferences {
-  /** Preferences accessible in the `glassdock` command */
+  /** Preferences accessible in the `glass-dock` command */
   export type GlassDock = ExtensionPreferences & {}
 }
 
 declare namespace Arguments {
-  /** Arguments passed to the `glassdock` command */
+  /** Arguments passed to the `glass-dock` command */
   export type GlassDock = {}
 }
