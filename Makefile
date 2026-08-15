naaiyy/glassdock

feat(app): add menu bar and Raycast control clients
Signed-off-by: naaiyy <naaiyyyy@gmail.com>

Makefile
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 BUILD_CONFIGURATION ?= debug
+APP_RELEASE_VERSION ?= 1.3.0
+APP_BUILD_NUMBER ?= 1
 TEST_PARALLELISM ?= $(shell sysctl -n hw.perflevel0.logicalcpu 2>/dev/null || sysctl -n hw.ncpu)
 TEST_SWIFT_FLAGS ?= --disable-index-store
 
@@ -66,6 +68,13 @@ help:
 	@echo "  all              - Build socktainer (default)"
 	@echo "  build            - Build in debug mode"
 	@echo "  release          - Build in release mode"
+	@echo "  control          - Build socktainerctl"
+	@echo "  menu-app         - Build a local Socktainer.app bundle"
+	@echo "  menu-popover-test - Verify the built menu-bar popover is visible"
+	@echo "  menu-release     - Prepare a Developer ID-signed app archive without submission"
+	@echo "  publishing-validate - Validate local Apple and Raycast publishing inputs"
+	@echo "  raycast-install  - Install Raycast extension dependencies"
+	@echo "  raycast-build    - Build and lint the Raycast extension"
 	@echo "  test             - Run tests"
 	@echo "  integration      - Run live Docker lifecycle integration tests"
 	@echo "  benchmark-preflight - Validate runtime benchmark configuration"
@@ -85,6 +94,45 @@ help:
 test: lint-pipes benchmark-test
 	@$(SWIFT) test -c $(BUILD_CONFIGURATION) $(TEST_SWIFT_FLAGS) \
 		--experimental-maximum-parallelization-width $(TEST_PARALLELISM)
+
+.PHONY: control
+control:
+	@$(SWIFT) build -c $(BUILD_CONFIGURATION) --product socktainerctl
+
+.PHONY: menu-app
+menu-app:
+	@bash scripts/build-menu-app.sh $(BUILD_CONFIGURATION) $(BUILD_VERSION)
+
+.PHONY: menu-popover-test
+menu-popover-test: menu-app
+	@bash scripts/test-menu-popover.sh .build/$(BUILD_CONFIGURATION)/Socktainer.app/Contents/MacOS/SocktainerMenu
+
+.PHONY: menu-release
+menu-release:
+	@bash scripts/prepare-menu-release.sh \
+		--version "$(APP_RELEASE_VERSION)" \
+		--build "$(APP_BUILD_NUMBER)"
+
+.PHONY: publishing-validate
+publishing-validate:
+	@plutil -lint \
+		Apps/SocktainerMenu/Info.plist \
+		Apps/SocktainerMenu/SocktainerMenu.entitlements \
+		Apps/SocktainerMenu/PrivacyInfo.xcprivacy >/dev/null
+	@jq -e . Apps/SocktainerMenu/Distribution.json raycast/package.json >/dev/null
+	@codesign --verify --deep --strict .build/release/Socktainer.app
+	@unzip -tq ".build/release-distribution/Socktainer-$(APP_RELEASE_VERSION)-macOS-arm64.zip" >/dev/null
+	@npm --prefix raycast run lint
+	@npm --prefix raycast run build
+
+.PHONY: raycast-install
+raycast-install:
+	@npm --prefix raycast install
+
+.PHONY: raycast-build
+raycast-build:
+	@npm --prefix raycast run lint
+	@npm --prefix raycast run build
 
 .PHONY: integration
 integration:

Package.swift
@@ -12,6 +12,13 @@ let package = Package(
     platforms: [
         .macOS(.v15)
     ],
+    products: [
+        .library(name: "SocktainerControl", targets: ["SocktainerControl"]),
+        .library(name: "SocktainerMenuKit", targets: ["SocktainerMenuKit"]),
+        .executable(name: "socktainer", targets: ["socktainer"]),
+        .executable(name: "socktainerctl", targets: ["socktainerctl"]),
+        .executable(name: "SocktainerMenu", targets: ["SocktainerMenu"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/container.git", exact: "1.2.1"),
         .package(url: "https://github.com/apple/containerization.git", exact: "0.40.1"),
@@ -22,6 +29,9 @@ let package = Package(
         .package(url: "https://github.com/facebook/zstd.git", exact: "1.5.7"),
     ],
     targets: [
+        .target(
+            name: "SocktainerControl"
+        ),
         .executableTarget(
             name: "socktainer",
             dependencies: [
@@ -45,6 +55,21 @@ let package = Package(
                 "BuildInfo",
             ]
         ),
+        .executableTarget(
+            name: "socktainerctl",
+            dependencies: [
+                "SocktainerControl",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
+        .executableTarget(
+            name: "SocktainerMenu",
+            dependencies: ["SocktainerMenuKit"]
+        ),
+        .target(
+            name: "SocktainerMenuKit",
+            dependencies: ["SocktainerControl"]
+        ),
         .testTarget(
             name: "socktainerTests",
             dependencies: [
@@ -53,6 +78,14 @@ let package = Package(
                 .product(name: "VaporTesting", package: "vapor"),
                 .product(name: "libzstd", package: "zstd"),
             ],
+        ),
+        .testTarget(
+            name: "SocktainerControlTests",
+            dependencies: ["SocktainerControl"]
+        ),
+        .testTarget(
+            name: "SocktainerMenuKitTests",
+            dependencies: ["SocktainerMenuKit"]
         ),
         .target(
             name: "CFilteredStream",

Tests/SocktainerMenuKitTests/StatusPopoverControllerTests.swift
@@ -1,0 +1,96 @@
+import Foundation
+import Testing
+
+@testable import SocktainerMenuKit
+
+@Suite("Menu-bar popover presentation")
+@MainActor
+struct StatusPopoverControllerTests {
+    @Test("content leaves the native popover material unobstructed")
+    func contentUsesNativePopoverMaterial() throws {
+        let repository = URL(fileURLWithPath: #filePath)
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+        let source = try String(
+            contentsOf: repository.appending(path: "Sources/SocktainerMenuKit/MenuViews.swift"),
+            encoding: .utf8
+        )
+        let popoverSource = try #require(
+            source.split(separator: "private struct PopoverHeader", maxSplits: 1).first
+        )
+
+        #expect(!popoverSource.contains(".background("))
+    }
+
+    @Test("containers are the default destination")
+    func containersAreDefault() {
+        let model = MenuModel()
+
+        #expect(model.selectedSection == .containers)
+    }
+
+    @Test("status-item activation toggles one anchored popover")
+    func togglesPopover() {
+        let popover = TestPopover()
+        let focus = TestApplicationFocus()
+        let controller = StatusPopoverController(popover: popover, applicationFocus: focus)
+
+        controller.togglePopover()
+
+        #expect(popover.showCount == 1)
+        #expect(popover.closeCount == 0)
+        #expect(focus.activateCount == 1)
+
+        controller.togglePopover()
+        controller.didClosePopover()
+
+        #expect(popover.showCount == 1)
+        #expect(popover.closeCount == 1)
+        #expect(focus.restoreCount == 1)
+    }
+
+    @Test("an outside-click close restores the previous application")
+    func restoresFocusAfterTransientClose() {
+        let popover = TestPopover()
+        let focus = TestApplicationFocus()
+        let controller = StatusPopoverController(popover: popover, applicationFocus: focus)
+
+        controller.togglePopover()
+        popover.isShown = false
+        controller.didClosePopover()
+
+        #expect(focus.restoreCount == 1)
+    }
+}
+
+@MainActor
+private final class TestPopover: StatusPopoverPresenting {
+    var isShown = false
+    var showCount = 0
+    var closeCount = 0
+
+    func show() {
+        showCount += 1
+        isShown = true
+    }
+
+    func close() {
+        closeCount += 1
+        isShown = false
+    }
+}
+
+@MainActor
+private final class TestApplicationFocus: ApplicationFocusControlling {
+    var activateCount = 0
+    var restoreCount = 0
+
+    func activateForPopover() {
+        activateCount += 1
+    }
+
+    func restorePreviousApplication() {
+        restoreCount += 1
+    }
+}

raycast/raycast-env.d.ts
@@ -1,0 +1,25 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** socktainerctl Executable - Optional absolute path to socktainerctl. Standard install locations are detected automatically. */
+  "controlExecutable"?: string
+}
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `socktainer` command */
+  export type Socktainer = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `socktainer` command */
+  export type Socktainer = {}
+}

scripts/build-menu-app.sh
@@ -1,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+
+configuration="${1:-debug}"
+version="${2:-0.0.0-dev}"
+build_number="${3:-1}"
+signing_identity="${4:--}"
+bundle_version="${version#v}"
+bundle_version="${bundle_version%%-*}"
+if [[ ! "$bundle_version" =~ ^[0-9]+(\.[0-9]+){0,2}$ ]]; then
+    bundle_version="0.0.0"
+fi
+if [[ ! "$build_number" =~ ^[0-9]+(\.[0-9]+){0,2}$ ]]; then
+    echo "Error: build number must contain one to three numeric components." >&2
+    exit 1
+fi
+root_dir="$(cd "$(dirname "$0")/.." && pwd)"
+output_dir="$root_dir/.build/$configuration/Socktainer.app"
+
+swift build --package-path "$root_dir" -c "$configuration" --product SocktainerMenu
+binary_path="$(swift build --package-path "$root_dir" -c "$configuration" --show-bin-path)/SocktainerMenu"
+
+rm -rf "$output_dir"
+mkdir -p "$output_dir/Contents/MacOS" "$output_dir/Contents/Resources"
+cp "$root_dir/Apps/SocktainerMenu/Info.plist" "$output_dir/Contents/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $bundle_version" "$output_dir/Contents/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $build_number" "$output_dir/Contents/Info.plist"
+cp "$binary_path" "$output_dir/Contents/MacOS/SocktainerMenu"
+bash "$root_dir/scripts/generate-app-icon.sh" "$output_dir/Contents/Resources/AppIcon.icns" >/dev/null
+cp "$root_dir/Apps/SocktainerMenu/PrivacyInfo.xcprivacy" "$output_dir/Contents/Resources/PrivacyInfo.xcprivacy"
+xattr -cr "$output_dir"
+
+if [[ "$signing_identity" == "-" ]]; then
+    codesign --force --sign - "$output_dir"
+else
+    codesign --force --timestamp --options runtime \
+        --entitlements "$root_dir/Apps/SocktainerMenu/SocktainerMenu.entitlements" \
+        --sign "$signing_identity" "$output_dir"
+fi
+codesign --verify --deep --strict "$output_dir"
+
+echo "$output_dir"

scripts/prepare-menu-release.sh
@@ -1,0 +1,77 @@
+#!/bin/bash
+
+set -euo pipefail
+
+root_dir="$(cd "$(dirname "$0")/.." && pwd)"
+version=""
+build_number=""
+signing_identity="${SOCKTAINER_SIGNING_IDENTITY:-}"
+
+usage() {
+    echo "Usage: $0 --version VERSION --build BUILD [--identity SHA1_OR_NAME]" >&2
+}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --version)
+            version="${2:-}"
+            shift 2
+            ;;
+        --build)
+            build_number="${2:-}"
+            shift 2
+            ;;
+        --identity)
+            signing_identity="${2:-}"
+            shift 2
+            ;;
+        *)
+            usage
+            exit 2
+            ;;
+    esac
+done
+
+if [[ ! "$version" =~ ^[0-9]+(\.[0-9]+){1,2}$ ]]; then
+    echo "Error: version must be a stable numeric version such as 1.0.0." >&2
+    exit 1
+fi
+if [[ ! "$build_number" =~ ^[0-9]+(\.[0-9]+){0,2}$ ]]; then
+    echo "Error: build must contain one to three numeric components." >&2
+    exit 1
+fi
+
+if [[ -z "$signing_identity" ]]; then
+    signing_identity="$(security find-identity -v -p codesigning | awk '/"Developer ID Application:/{print $2}')"
+    identity_count="$(printf '%s\n' "$signing_identity" | awk 'NF { count++ } END { print count + 0 }')"
+    if [[ "$identity_count" -ne 1 ]]; then
+        echo "Error: specify one Developer ID Application identity with --identity." >&2
+        exit 1
+    fi
+fi
+
+app_path="$root_dir/.build/release/Socktainer.app"
+distribution_dir="$root_dir/.build/release-distribution"
+archive_path="$distribution_dir/Socktainer-$version-macOS-arm64.zip"
+
+bash "$root_dir/scripts/build-menu-app.sh" release "$version" "$build_number" "$signing_identity"
+mkdir -p "$distribution_dir"
+
+codesign --verify --deep --strict --verbose=2 "$app_path"
+if codesign --display --entitlements :- "$app_path" 2>/dev/null | plutil -extract com.apple.security.get-task-allow raw - 2>/dev/null | grep -qx true; then
+    echo "Error: release app contains com.apple.security.get-task-allow." >&2
+    exit 1
+fi
+
+ditto -c -k --keepParent "$app_path" "$archive_path"
+unzip -tq "$archive_path" >/dev/null
+
+if spctl --assess --type execute --verbose=2 "$app_path" >/dev/null 2>&1; then
+    echo "Gatekeeper accepted the pre-notarization app."
+else
+    echo "Gatekeeper did not accept the pre-notarization app. This result is expected before notarization."
+fi
+
+echo "Prepared signed archive without submitting it:"
+echo "$archive_path"
+shasum -a 256 "$archive_path"

socktainer/socktainer

Makefile
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 BUILD_CONFIGURATION ?= debug
+APP_RELEASE_VERSION ?= 1.3.0
+APP_BUILD_NUMBER ?= 1
 TEST_PARALLELISM ?= $(shell sysctl -n hw.perflevel0.logicalcpu 2>/dev/null || sysctl -n hw.ncpu)
 TEST_SWIFT_FLAGS ?= --disable-index-store
 
@@ -66,6 +68,13 @@ help:
 	@echo "  all              - Build glassdock (default)"
 	@echo "  build            - Build in debug mode"
 	@echo "  release          - Build in release mode"
+	@echo "  control          - Build glassdockctl"
+	@echo "  menu-app         - Build a local GlassDock.app bundle"
+	@echo "  menu-popover-test - Verify the built menu-bar popover is visible"
+	@echo "  menu-release     - Prepare a Developer ID-signed app archive without submission"
+	@echo "  publishing-validate - Validate local Apple and Raycast publishing inputs"
+	@echo "  raycast-install  - Install Raycast extension dependencies"
+	@echo "  raycast-build    - Build and lint the Raycast extension"
 	@echo "  test             - Run tests"
 	@echo "  integration      - Run live Docker lifecycle integration tests"
 	@echo "  benchmark-preflight - Validate runtime benchmark configuration"
@@ -85,6 +94,45 @@ help:
 test: lint-pipes benchmark-test
 	@$(SWIFT) test -c $(BUILD_CONFIGURATION) $(TEST_SWIFT_FLAGS) \
 		--experimental-maximum-parallelization-width $(TEST_PARALLELISM)
+
+.PHONY: control
+control:
+	@$(SWIFT) build -c $(BUILD_CONFIGURATION) --product glassdockctl
+
+.PHONY: menu-app
+menu-app:
+	@bash scripts/build-menu-app.sh $(BUILD_CONFIGURATION) $(BUILD_VERSION)
+
+.PHONY: menu-popover-test
+menu-popover-test: menu-app
+	@bash scripts/test-menu-popover.sh .build/$(BUILD_CONFIGURATION)/GlassDock.app/Contents/MacOS/GlassDockMenu
+
+.PHONY: menu-release
+menu-release:
+	@bash scripts/prepare-menu-release.sh \
+		--version "$(APP_RELEASE_VERSION)" \
+		--build "$(APP_BUILD_NUMBER)"
+
+.PHONY: publishing-validate
+publishing-validate:
+	@plutil -lint \
+		Apps/GlassDockMenu/Info.plist \
+		Apps/GlassDockMenu/GlassDockMenu.entitlements \
+		Apps/GlassDockMenu/PrivacyInfo.xcprivacy >/dev/null
+	@jq -e . Apps/GlassDockMenu/Distribution.json raycast/package.json >/dev/null
+	@codesign --verify --deep --strict .build/release/GlassDock.app
+	@unzip -tq ".build/release-distribution/GlassDock-$(APP_RELEASE_VERSION)-macOS-arm64.zip" >/dev/null
+	@npm --prefix raycast run lint
+	@npm --prefix raycast run build
+
+.PHONY: raycast-install
+raycast-install:
+	@npm --prefix raycast install
+
+.PHONY: raycast-build
+raycast-build:
+	@npm --prefix raycast run lint
+	@npm --prefix raycast run build
 
 .PHONY: integration
 integration:

Package.swift
@@ -13,7 +13,11 @@ let package = Package(
         .macOS(.v15)
     ],
     products: [
-        .executable(name: "glassdock", targets: ["GlassDock"])
+        .library(name: "GlassDockControl", targets: ["GlassDockControl"]),
+        .library(name: "GlassDockMenuKit", targets: ["GlassDockMenuKit"]),
+        .executable(name: "glassdock", targets: ["GlassDock"]),
+        .executable(name: "glassdockctl", targets: ["glassdockctl"]),
+        .executable(name: "GlassDockMenu", targets: ["GlassDockMenu"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/container.git", exact: "1.2.1"),
@@ -25,6 +29,9 @@ let package = Package(
         .package(url: "https://github.com/facebook/zstd.git", exact: "1.5.7"),
     ],
     targets: [
+        .target(
+            name: "GlassDockControl"
+        ),
         .executableTarget(
             name: "GlassDock",
             dependencies: [
@@ -48,6 +55,21 @@ let package = Package(
                 "BuildInfo",
             ]
         ),
+        .executableTarget(
+            name: "glassdockctl",
+            dependencies: [
+                "GlassDockControl",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
+        .executableTarget(
+            name: "GlassDockMenu",
+            dependencies: ["GlassDockMenuKit"]
+        ),
+        .target(
+            name: "GlassDockMenuKit",
+            dependencies: ["GlassDockControl"]
+        ),
         .testTarget(
             name: "GlassDockTests",
             dependencies: [
@@ -56,6 +78,14 @@ let package = Package(
                 .product(name: "VaporTesting", package: "vapor"),
                 .product(name: "libzstd", package: "zstd"),
             ],
+        ),
+        .testTarget(
+            name: "GlassDockControlTests",
+            dependencies: ["GlassDockControl"]
+        ),
+        .testTarget(
+            name: "GlassDockMenuKitTests",
+            dependencies: ["GlassDockMenuKit"]
         ),
         .target(
             name: "CFilteredStream",
@@ -78,5 +108,4 @@ let package = Package(
             ]
         ),
     ]
-
 )

Tests/GlassDockMenuKitTests/StatusPopoverControllerTests.swift
@@ -1,0 +1,96 @@
+import Foundation
+import Testing
+
+@testable import GlassDockMenuKit
+
+@Suite("Menu-bar popover presentation")
+@MainActor
+struct StatusPopoverControllerTests {
+    @Test("content leaves the native popover material unobstructed")
+    func contentUsesNativePopoverMaterial() throws {
+        let repository = URL(fileURLWithPath: #filePath)
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+        let source = try String(
+            contentsOf: repository.appending(path: "Sources/GlassDockMenuKit/MenuViews.swift"),
+            encoding: .utf8
+        )
+        let popoverSource = try #require(
+            source.split(separator: "private struct PopoverHeader", maxSplits: 1).first
+        )
+
+        #expect(!popoverSource.contains(".background("))
+    }
+
+    @Test("containers are the default destination")
+    func containersAreDefault() {
+        let model = MenuModel()
+
+        #expect(model.selectedSection == .containers)
+    }
+
+    @Test("status-item activation toggles one anchored popover")
+    func togglesPopover() {
+        let popover = TestPopover()
+        let focus = TestApplicationFocus()
+        let controller = StatusPopoverController(popover: popover, applicationFocus: focus)
+
+        controller.togglePopover()
+
+        #expect(popover.showCount == 1)
+        #expect(popover.closeCount == 0)
+        #expect(focus.activateCount == 1)
+
+        controller.togglePopover()
+        controller.didClosePopover()
+
+        #expect(popover.showCount == 1)
+        #expect(popover.closeCount == 1)
+        #expect(focus.restoreCount == 1)
+    }
+
+    @Test("an outside-click close restores the previous application")
+    func restoresFocusAfterTransientClose() {
+        let popover = TestPopover()
+        let focus = TestApplicationFocus()
+        let controller = StatusPopoverController(popover: popover, applicationFocus: focus)
+
+        controller.togglePopover()
+        popover.isShown = false
+        controller.didClosePopover()
+
+        #expect(focus.restoreCount == 1)
+    }
+}
+
+@MainActor
+private final class TestPopover: StatusPopoverPresenting {
+    var isShown = false
+    var showCount = 0
+    var closeCount = 0
+
+    func show() {
+        showCount += 1
+        isShown = true
+    }
+
+    func close() {
+        closeCount += 1
+        isShown = false
+    }
+}
+
+@MainActor
+private final class TestApplicationFocus: ApplicationFocusControlling {
+    var activateCount = 0
+    var restoreCount = 0
+
+    func activateForPopover() {
+        activateCount += 1
+    }
+
+    func restorePreviousApplication() {
+        restoreCount += 1
+    }
+}

raycast/raycast-env.d.ts
@@ -1,0 +1,25 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** glassdockctl Executable - Optional absolute path to glassdockctl. Standard install locations are detected automatically. */
+  "controlExecutable"?: string
+}
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `glassdock` command */
+  export type GlassDock = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `glassdock` command */
+  export type GlassDock = {}
+}

scripts/build-menu-app.sh
@@ -1,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+
+configuration="${1:-debug}"
+version="${2:-0.0.0-dev}"
+build_number="${3:-1}"
+signing_identity="${4:--}"
+bundle_version="${version#v}"
+bundle_version="${bundle_version%%-*}"
+if [[ ! "$bundle_version" =~ ^[0-9]+(\.[0-9]+){0,2}$ ]]; then
+    bundle_version="0.0.0"
+fi
+if [[ ! "$build_number" =~ ^[0-9]+(\.[0-9]+){0,2}$ ]]; then
+    echo "Error: build number must contain one to three numeric components." >&2
+    exit 1
+fi
+root_dir="$(cd "$(dirname "$0")/.." && pwd)"
+output_dir="$root_dir/.build/$configuration/GlassDock.app"
+
+swift build --package-path "$root_dir" -c "$configuration" --product GlassDockMenu
+binary_path="$(swift build --package-path "$root_dir" -c "$configuration" --show-bin-path)/GlassDockMenu"
+
+rm -rf "$output_dir"
+mkdir -p "$output_dir/Contents/MacOS" "$output_dir/Contents/Resources"
+cp "$root_dir/Apps/GlassDockMenu/Info.plist" "$output_dir/Contents/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $bundle_version" "$output_dir/Contents/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $build_number" "$output_dir/Contents/Info.plist"
+cp "$binary_path" "$output_dir/Contents/MacOS/GlassDockMenu"
+bash "$root_dir/scripts/generate-app-icon.sh" "$output_dir/Contents/Resources/AppIcon.icns" >/dev/null
+cp "$root_dir/Apps/GlassDockMenu/PrivacyInfo.xcprivacy" "$output_dir/Contents/Resources/PrivacyInfo.xcprivacy"
+xattr -cr "$output_dir"
+
+if [[ "$signing_identity" == "-" ]]; then
+    codesign --force --sign - "$output_dir"
+else
+    codesign --force --timestamp --options runtime \
+        --entitlements "$root_dir/Apps/GlassDockMenu/GlassDockMenu.entitlements" \
+        --sign "$signing_identity" "$output_dir"
+fi
+codesign --verify --deep --strict "$output_dir"
+
+echo "$output_dir"

scripts/prepare-menu-release.sh
@@ -1,0 +1,77 @@
+#!/bin/bash
+
+set -euo pipefail
+
+root_dir="$(cd "$(dirname "$0")/.." && pwd)"
+version=""
+build_number=""
+signing_identity="${SOCKTAINER_SIGNING_IDENTITY:-}"
+
+usage() {
+    echo "Usage: $0 --version VERSION --build BUILD [--identity SHA1_OR_NAME]" >&2
+}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --version)
+            version="${2:-}"
+            shift 2
+            ;;
+        --build)
+            build_number="${2:-}"
+            shift 2
+            ;;
+        --identity)
+            signing_identity="${2:-}"
+            shift 2
+            ;;
+        *)
+            usage
+            exit 2
+            ;;
+    esac
+done
+
+if [[ ! "$version" =~ ^[0-9]+(\.[0-9]+){1,2}$ ]]; then
+    echo "Error: version must be a stable numeric version such as 1.0.0." >&2
+    exit 1
+fi
+if [[ ! "$build_number" =~ ^[0-9]+(\.[0-9]+){0,2}$ ]]; then
+    echo "Error: build must contain one to three numeric components." >&2
+    exit 1
+fi
+
+if [[ -z "$signing_identity" ]]; then
+    signing_identity="$(security find-identity -v -p codesigning | awk '/"Developer ID Application:/{print $2}')"
+    identity_count="$(printf '%s\n' "$signing_identity" | awk 'NF { count++ } END { print count + 0 }')"
+    if [[ "$identity_count" -ne 1 ]]; then
+        echo "Error: specify one Developer ID Application identity with --identity." >&2
+        exit 1
+    fi
+fi
+
+app_path="$root_dir/.build/release/GlassDock.app"
+distribution_dir="$root_dir/.build/release-distribution"
+archive_path="$distribution_dir/GlassDock-$version-macOS-arm64.zip"
+
+bash "$root_dir/scripts/build-menu-app.sh" release "$version" "$build_number" "$signing_identity"
+mkdir -p "$distribution_dir"
+
+codesign --verify --deep --strict --verbose=2 "$app_path"
+if codesign --display --entitlements :- "$app_path" 2>/dev/null | plutil -extract com.apple.security.get-task-allow raw - 2>/dev/null | grep -qx true; then
+    echo "Error: release app contains com.apple.security.get-task-allow." >&2
+    exit 1
+fi
+
+ditto -c -k --keepParent "$app_path" "$archive_path"
+unzip -tq "$archive_path" >/dev/null
+
+if spctl --assess --type execute --verbose=2 "$app_path" >/dev/null 2>&1; then
+    echo "Gatekeeper accepted the pre-notarization app."
+else
+    echo "Gatekeeper did not accept the pre-notarization app. This result is expected before notarization."
+fi
+
+echo "Prepared signed archive without submitting it:"
+echo "$archive_path"
+shasum -a 256 "$archive_path"